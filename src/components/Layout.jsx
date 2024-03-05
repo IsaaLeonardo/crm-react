f170737536a89386b0fa7f808cd1b9e34e486c26
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-03-05 17:18:33 -0400

    Create layout styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,14 @@ import { Outlet } from "react-router-dom"
 
 function Layout() {
   return (
-    <div>
-      <h1 className="text-6xl">CRM - Clientes</h1>
+    <div className="md:flex md:min-h-screen">
+      <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h1 className="text-white font-black text-center">CRM - Clientes</h1>
+      </aside>
 
-      <Outlet />
+      <main className="md:w-3/4 p-10 overflow-scroll">
+        <Outlet />
+      </main>
     </div>
   )
 }
