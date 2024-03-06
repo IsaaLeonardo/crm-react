2cc6c27709f37c1d21ec1571ab04c6d92127c4d0
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-03-06 14:19:46 -0400

    Create a navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,31 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 function Layout() {
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
-        <h1 className="text-white font-black text-center">CRM - Clientes</h1>
+        <h1 className="text-2xl text-white font-black text-center">CRM - Clientes</h1>
+
+        <nav className="mt-10">
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className="block text-xl text-white hover:text-blue-200"
+              >
+                Clientes
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/clientes/nuevo"
+                className="block text-xl text-white hover:text-blue-200"
+              >
+                Nuevo Cliente
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </aside>
 
       <main className="md:w-3/4 p-10 overflow-scroll">
